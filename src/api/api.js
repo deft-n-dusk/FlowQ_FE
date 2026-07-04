e0da33cf28b186803c52287a953ae459059b84d0
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2026-07-04 22:32:45 +0530

    updated

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:2713/api",
+  baseURL: "https://flowq-be.onrender.com",
 });
 
 export const getJobs = (params = {}) =>
   API.get("/jobs", { params });
 
 export const getJobById = async (jobId) => {
-  return await axios.get(`/api/jobs/${jobId}`); 
+  return await API.get(`/jobs/${jobId}`); 
 };
 
 export const createJob = (data) =>
